8fa752b52b3186ba6088468a3d259b139bb56d90
Iman <imanhamzah111@gmail.com>
2019-03-07 20:37:22 +0800

    commit1

---
+++ bulk-email-proc.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+//include 'includes/header.php';
+require 'vendor/autoload.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $emails = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $mail = new PHPMailer(true);              // Passing `true` enables exceptions
+    try {
+        //Server settings
+        // $mail->SMTPDebug = 1;                 // Enable verbose debug output
+        $mail->isSMTP();                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';                     // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;               // Enable SMTP authentication
+        $mail->Username = 'lithanlithan8@gmail.com';                 // SMTP username
+        $mail->Password = 'H245hyt12';                 // SMTP password
+        $mail->SMTPSecure = 'tls';            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                    // TCP port to connect to
+        
+        //Recipients
+        $mail->setFrom('imanhamzah111@gmail.com', 'Iman');
+        // Add each email into the $mail object      
+        $email_recipients = explode(',',$emails);
+        foreach ($email_recipients as $recipient) {
+            $mail->addAddress($recipient);
+        }
+        //$mail->addReplyTo('jameshoty@me.com', 'Support');
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = $message;
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->send();
+        echo "<br>";
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    }
+}
+//include 'includes/footer.php';
+?>
---
+++ bulksend.php
@@ -0,0 +1,51 @@
+<?php
+if(isset($_POST["submitted"])){
+    $email=strip_tags($_POST["email"]);
+    $message=strip_tags($_POST["message"]);
+}
+
+
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+
+    
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';                        // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'lithanlithan8@gmail.com';                 // SMTP username
+    $mail->Password = 'H245hyt12';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
+    
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('imanhamzah111@gmail.com', 'Joe User');     // Add a recipient
+    $mail->addAddress('jameshoty@me.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    
+    
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+    echo $email;
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
+?>
